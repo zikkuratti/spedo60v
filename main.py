554dc96aee2b4f829556b9db21d6fae8e68a3a42
z1kkuratt1 <41235726+zikkuratti@users.noreply.github.com>
2023-04-26 15:03:32 +0600

    refactored+time correction

--- main.py
+++ main.py
@@ -1,73 +1,63 @@
- 
-# Импорт всех классов
+import datetime
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
-
 from kivy.core.window import Window
- 
-# Глобальные настройки
-#-- maximize first, to get the screen size, minus any OS toolbars
-Window.maximize()
-maxSize = Window.system_size
-
-#-- set the actual window size, to be slightly smaller than full screen
-desiredSize = (maxSize[0]*0.9, maxSize[1]*0.9)
-Window.size = desiredSize
-
-#-- center the window
-Window.left = (maxSize[0] - desiredSize[0])*0.5
-Window.top = (maxSize[1] - desiredSize[1])*0.5
 
 
+class Converter(App):
 
-##Window.size = (250, 200)
-Window.clearcolor = (62 / 255, 17 / 255, 92 / 255, 1)
-Window.title = "Конвертер"
-
-
-class MyApp(App):
-
-    # Создание всех виджетов (объектов)
-    def __init__(self):
-        super().__init__()
-        self.charge = Label(text='Charge')
-        self.oncharge = Label(text='Time to charge')
-        self.trip = Label(text='Time to trip')
-        self.distance = Label(text='Got Distance')
-        self.input_data = TextInput(hint_text='How much volts?', multiline=False)
-        self.input_data.bind(text=self.on_text)  # Добавляем обработчик события
-
-    # Получаем данные и производит их конвертацию
-    def on_text(self, *args):
-        data = self.input_data.text
-
-        if data:
-            #19.2 разброс верх67.2 низ48 для 60v   (в твоем случае 36 30 вроде как 6 но хз подогнать надо билд)
-            self.charge.text = 'Заряда осталось: ' + str(round((((float(data.replace(',','.'))-48) * 100)/19.2), 2)) + ' % батареи'
-            #переменная вычесленного процента заряда чтоб дальше юзать
-            percentcharged = str((((float(data.replace(',','.'))-48) * 100)/19.2))
-            #по фармуле амперчасы батареи делим на амперы зарядки и умножаем на коэффицент от 1.2 до 1.6 вычисляется экспериментально наблюдением за сколько половина батки зарядится в моем случае 1.45*(21.5\3)
-            #11.5 подгоняю до 10.75
-            self.oncharge.text = 'Время на подзарядку: ' + str(round(10.75 - (10.75 * (float(percentcharged)/100)), 2))+ ' часа'
-            # экспериментально самик на ходу 4.5 часа умножаем на процент заряда
-            self.trip.text = 'Остаточное время пути: ' + str(round( (4.5 * (float(percentcharged)/100)), 2)) + ' часа'
-            #экспериментально самик проходит 64км домножаем на процент заряда
-            self.distance.text = 'Остаточное расстояние: ' + str(round( (64 * (float(percentcharged)/100)), 2)) + ' км'
-
-    # Основной метод для построения программы
     def build(self):
-        # Все объекты будем помещать в один общий слой
+        Window.clearcolor = (62 / 255, 17 / 255, 92 / 255, 1)
+        Window.title = "Конвертер"
+
+        # maximize first, to get the screen size, minus any OS toolbars
+        Window.maximize()
+        # set the actual window size, to be slightly smaller than full screen
+        maxSize = Window.system_size
+        desiredSize = (maxSize[0]*0.9, maxSize[1]*0.9)
+        Window.size = desiredSize
+        # center the window
+        Window.left = (maxSize[0] - desiredSize[0])*0.5
+        Window.top = (maxSize[1] - desiredSize[1])*0.5
+
+        self.charge = Label(text='Заряда осталось:')
+        self.oncharge = Label(text='Время на подзарядку:')
+        self.trip = Label(text='Остаточное время пути:')
+        self.distance = Label(text='Остаточное расстояние:')
+        self.input_data = TextInput(hint_text='How much volts?', multiline=False)
+        self.input_data.bind(text=self.on_text)
+
         box = BoxLayout(orientation='vertical')
         box.add_widget(self.input_data)
         box.add_widget(self.charge)
         box.add_widget(self.oncharge)
         box.add_widget(self.trip)
         box.add_widget(self.distance)
+
         return box
 
+    def on_text(self, *args):
+        data = self.input_data.text
 
-# Запуск проекта
+        if data:
+            data = float(data.replace(',', '.'))
+            percentcharged = ((data - 48) * 100) / 19.2
+            self.charge.text = f'Заряда осталось: {percentcharged:.2f} % батареи'
+
+            # амперчасы батареи делим на амперы зарядки и умножаем на коэффицент от 1.2 до 1.6
+            charging_time = round(10.75 - (10.75 * (percentcharged/100)), 2)
+            self.oncharge.text = f'Время на подзарядку: {datetime.timedelta(hours=charging_time)}'
+
+            # время на ходу 4.5 часа умножаем на процент заряда
+            trip_time = round(4.5 * (percentcharged/100), 2)
+            self.trip.text = f'Остаточное время пути: {datetime.timedelta(hours=trip_time)}'
+
+            # самокат проходит 64 км, домножаем на процент заряда
+            remaining_distance = round(64 * (percentcharged/100), 2)
+            self.distance.text = f'Остаточное расстояние: {remaining_distance} км'
+
+
 if __name__ == "__main__":
-    MyApp().run()
+    Converter().run()
